8f58aa5d75de9d5394cd5996c507e44449346747
saulgs <sgs319@hotmail.com>
2017-06-25 23:29:31 -0600

    Add files via upload

---
+++ Form_panel_de_entregas/index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">	
+		<form class="navbar-form navbar-right">
+  			<div class="input-group">
+    			<input type="text" class="form-control" placeholder="Search">
+    				<div class="input-group-btn">
+     					 <button class="btn btn-default" type="submit">
+       						 <i class="glyphicon glyphicon-search"></i>
+     					 </button>
+   					 </div>
+  			</div>
+  		</form>	
+ </div>
+ </nav> 
+	
+	<form action="index.php" method="GET">	
+			<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+				<div class="well">
+					Ordenar por:<br><br>
+					<label><input type="checkbox" name="chk-categoria[]" id="chk-categoria[]" value="destino_final">Destino Final</label><br>
+					<label><input type="checkbox" name="chk-categoria[]" value="destinatario">Destinatario</label><br>
+					<label><input type="checkbox" name="chk-categoria[]" value="fecha">Fecha</label><br>
+					<label><input type="checkbox" name="chk-categoria[]" value="estado">Estado</label>
+				</div>
+			</div>
+	</form>
+<div class="col-xs-6 col-sm-6 col-md-4 col-lg-8">
+	<div class="well">
+		<table class="table table-bordered">
+			<tr>
+				<th>Articulo</th>
+				<th>Destino Final</th>
+				<th>Destino Origen</th>
+				<th>Distancia</th>
+				<th>Estado</th>
+				<th>Fecha</th>
+				<th>Costo</th>
+			</tr>
+		<tr>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+		</tr>
+		<tr>
+			<tr>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+		</tr>
+		<tr>
+			<tr>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+		</tr>
+		<tr>
+			<tr>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+		</tr>
+		<tr>
+			<tr>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+		</tr>
+		<tr>
+			<tr>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+			<td>
+				Lorem Ipsum
+			</td>
+		</tr>
+		</table>
+	</div>
+</div>
+	<script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
