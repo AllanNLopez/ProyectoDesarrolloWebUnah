19fdca36d3b0f7407ae256de91002cf9c67453d1
Allan N López <anlopez_1294@hotmail.com>
2017-10-04 01:23:11 -0600

    actualizacion octubre

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,5 +1,5 @@
 
-//funciones para ocultar y aparecer el sidebar
+/*   funciones para ocultar y aparecer el sidebar  */
 if (screen.width <480) {
     	document.getElementById("mySidenav").style.width = "0";
     	document.getElementById("main").style.marginLeft= "0";
@@ -11,9 +11,11 @@ if (screen.width <480) {
 
 function openNav() {
 	if (screen.width <480) {
-		document.getElementById("mySidenav").style.width = "100%";
-		document.getElementById("main").style.marginLeft = "250px";
+		document.getElementById("mySidenav").style.width = "100%"; 
 		document.getElementById("mySidenav").style.height = "100%";
+		document.getElementById("mySidenav").style.position = "fixed";
+		//document.getElementById("main").style.marginLeft = "250px";
+		//document.getElementById("main").style.display = "none";  
 	}else
 	{
 		document.getElementById("mySidenav").style.width = "250px";
@@ -25,5 +27,21 @@ function openNav() {
 function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
 	document.getElementById("main").style.marginLeft= "0";
+	//document.getElementById("main").style.display = "block";
 	$("#mySidenav").removeClass("sidenav-small");
+}
+
+
+/* Funciones de validacion */
+
+function validarTelefono(telefono){
+
+}
+
+function validarIdentidad(id){
+	var re = /\d{4}-\d{4}-\d{5}/;
+	if(re.test(id)){
+		return 1;
+	}else
+		return 0;
 }
