455aaef25c93c1b781e179ab1a0859a26302f05e
oavasquez <oavasquez01@gmail.com>
2017-10-14 02:17:37 -0600

    habilitar y deshabilitar campos y botones

--- js/user-index-ctl.js
+++ js/user-index-ctl.js
@@ -1,4 +1,41 @@
 
 $(document).ready(function() {
+	//####agregar funcionalidad de editar#######
+	$("#btnEditar").click(function(){
+		$('.campoUsuario').removeAttr("readonly");
+		habilitarbotones();
+		
+	});
+
+	$("#btnAceptar").click(function(){
+		$('.campoUsuario').attr("readonly",true);
+		dehabilitarbotones();
+		
+
+	});
+
+	$("#btnCancelar").click(function(){
+		$('.campoUsuario').attr("readonly",true);
+		dehabilitarbotones();
+		
+
+	});
+
+	function habilitarbotones(){
+		$("#btnAceptar").prop("disabled",false);
+		$("#btnCancelar").prop("disabled",false);
+
+	}
+
+	function dehabilitarbotones(){
+		$("#btnAceptar").prop("disabled",true);
+		$("#btnCancelar").prop("disabled",true);
+		
+	}
+
+
+
     $('table.display').DataTable(  ); 
 } );
+
+
