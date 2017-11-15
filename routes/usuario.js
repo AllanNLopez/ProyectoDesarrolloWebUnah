55393fe4bd17af1db32c227098ceb13591818cf9
Saul_Guzman <sgs319@hotmail.com>
2017-11-15 17:26:04 -0600

    Correccion rutas seguras

--- routes/usuario.js
+++ routes/usuario.js
@@ -13,7 +13,6 @@ router.use(bodyParser.json());
 var realizarQuery = require('../modulos/conexion').realizarQuery;
 
 router.post('/', urlEncodeParser, function(peticion,respuesta){
-	console.log("consiguiendo datos");
 	var sql = "SELECT codUsuario as codigo, nombres, apellidos, telefono, correo, contrasena FROM tblUsuarios WHERE codUsuario=?";
     var values = [peticion.body.codigo];
     realizarQuery(sql, values, function(data){
