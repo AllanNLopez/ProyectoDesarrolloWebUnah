3140b0ccc95b2c30c54df14a09551337a6613409
Allan N López <anlopez_1294@hotmail.com>
2017-11-15 21:11:56 -0600

    Cargar tiendas de usuario

--- routes/userTiendas.js
+++ routes/userTiendas.js
@@ -9,8 +9,48 @@ var urlEncodeParser = bodyParser.urlencoded({
 
 router.use(bodyParser.json());
 
+router.post("/getTiendas", urlEncodeParser, function(request, response){
+    var sql = "SELECT codEmpresa, nombreEmpresa from tblempresas where codUsuario = ?";
+    var values = [request.body.codigoUsuario];
+    realizarQuery(sql,values, function(res){
+      response.send(res);
+    });
+});
+
+router.post("/guardarArticulo", urlEncodeParser, function(request, response){
+  var sql = "INSERT INTO tblarticulos("
+                            +"codArticulo, "
+                            +"nombreArticulo, "
+                            +"descripcion, "
+                            +"precio, "
+                            +"origenFabricacion, "
+                            +"saldo, "
+                            +"fechaPublicacion, "
+                            +"estado, "
+                            +"codCategoria, "
+                           +" codUsuarioPublicador "
+                          +") "
+                +" VALUES (null,?,?,?,'Honduras',?,'NOW()',?,?,?)";
+  var values = [ 
+                request.body.nombreArticulo, 
+                request.body.descripcion, 
+                request.body.precio,  
+                request.body.saldo,  
+                request.body.estado, 
+                request.body.codCategoria, 
+                request.body.codUsuarioPublicador 
+              ];
+  realizarQuery(sql, values, function(res){
+    response.send(res);
+  })
+});
+
 router.get("/getTiendas", function(request, response){
-    response.send("hola");
+    var sql = "SELECT codEmpresa, nombreEmpresa from tblempresas where codUsuario = 2";
+    var values = [request.query.codigoUsuario];
+    realizarQuery(sql,values, function(res){
+      response.send(res);
+    });
 });
 
 module.exports = router;
